6bc74311ce384e93da37b23d602f6740b4d48cfa
Yiming <59931110+Yiming25@users.noreply.github.com>
2024-10-30 14:42:23 -0400

    Update kmeans_example.py

--- kmeans_example.py
+++ kmeans_example.py
@@ -1,47 +1,40 @@
-   import numpy as np​
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import argparse
+import os
 
-   from sklearn.cluster import KMeans​
+# Set up argument parsing
+parser = argparse.ArgumentParser(description="K-means clustering on a dataset specified by the user.")
+parser.add_argument('--dataset', type=str, required=True, help="Path to the CSV file containing the dataset.")
+parser.add_argument('--n_clusters', type=int, default=2, help="Number of clusters for K-means. Default is 2.")
+args = parser.parse_args()
 
-   import matplotlib.pyplot as plt​
+# Check if the file exists
+if not os.path.isfile(args.dataset):
+    raise FileNotFoundError(f"The specified file '{args.dataset}' does not exist.")
 
-   ​
+# Load the dataset from the provided CSV file
+data = pd.read_csv(args.dataset).values
 
-   # Example dataset​
+# K-means clustering
+kmeans = KMeans(n_clusters=args.n_clusters)
+kmeans.fit(data)
 
-   data = np.array([[1, 2], [2, 3], [3, 4], [5, 8], [8, 8], [9, 11]])​
+# Print the labels assigned to each data point
+print("Labels assigned to each data point:")
+print(kmeans.labels_)
 
-   ​
+# Print the coordinates of the cluster centers
+print("\nCoordinates of cluster centers:")
+print(kmeans.cluster_centers_)
 
-   # K-means clustering​
+# Print the inertia (sum of squared distances to the closest cluster center)
+print("\nInertia (sum of squared distances to the closest cluster center):")
+print(kmeans.inertia_)
 
-   kmeans = KMeans(n_clusters=2)​
-
-   kmeans.fit(data)​
-
-  # Print the labels assigned to each data point ​
-
-  print("Labels assigned to each data point:") ​
-
-  print(kmeans.labels_) ​
-
-  # Print the coordinates of the cluster centers ​
-
-  print("\nCoordinates of cluster centers:") ​
-
-  print(kmeans.cluster_centers_) ​
-
-  # Print the inertia (sum of squared distances to the closest cluster center) ​
-
-  print("\nInertia (sum of squared distances to the closest cluster center):") ​
-
-  print(kmeans.inertia_)​
-
-   ​
-
-   # Plotting the results​
-
-   plt.scatter(data[:, 0], data[:, 1], c=kmeans.labels_, cmap='rainbow')​
-
-   plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='black', marker='x')​
-
-   plt.show()
+# Plotting the results
+plt.scatter(data[:, 0], data[:, 1], c=kmeans.labels_, cmap='rainbow')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='black', marker='x')
+plt.show()
